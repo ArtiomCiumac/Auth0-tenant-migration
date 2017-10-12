c774439b2a6c437dbaa0c71faff48007dda2be58
Artiom Ciumac <artiom.ciumac@gmail.com>
2017-10-12 13:50:54 +0100

    Updated login script to read full user profile

--- database-connections/artex-old-import/login.js
+++ database-connections/artex-old-import/login.js
@@ -7,6 +7,11 @@ function login(email, password, callback) {
     const jwt = require('jsonwebtoken@7.1.9');
     const jwks = require('jwks-rsa@1.1.1');
 
+    const jwksClient = jwks({
+        cache: true,
+        jwksUri: configuration.domain + '/.well-known/jwks.json'
+    });
+
     // helpers
 
     function getTokenRequestOptions() {
@@ -26,6 +31,34 @@ function login(email, password, callback) {
         };
     }
 
+    function getClientCredentialsRequestOptions() {
+        return {
+            method: 'POST',
+            url: configuration.domain + '/oauth/token',
+            headers: { 'content-type': 'application/json' },
+            body:
+            {
+                client_id: configuration.client_id,
+                client_secret: configuration.client_secret,
+                audience: configuration.domain + '/api/v2/',
+                grant_type: 'client_credentials'
+            },
+            json: true
+        };
+    }
+
+    function getUserProfileRequestOptions(userId, token) {
+        return {
+            method: 'GET',
+            url: configuration.domain + '/api/v2/users/' + userId,
+            headers: {
+                'content-type': 'application/json',
+                Authorization: 'Bearer ' + token
+            },
+            json: true
+        };
+    }
+
     function validateTokenHeader(token) {
         if (!token || !token.header ||
             token.header.typ !== 'JWT' || token.header.alg !== 'RS256') {
@@ -37,36 +70,44 @@ function login(email, password, callback) {
 
     function getSigningKey(keyId) {
         return new Promise((resolve, reject) => {
-            const jwksClient = jwks({
-                jwksUri: configuration.domain + '/.well-known/jwks.json'
-            });
-
             jwksClient.getSigningKey(keyId, function (err, key) {
                 if (err) return reject(err);
                 return resolve(key.publicKey || key.rsaPublicKey);
             });
-        })
+        });
     }
 
-    function fixUserId(userId) {
-        return userId.replace(/^auth0\|/i, "");
+    function decodeAndVerifyToken(token) {
+        return Promise.resolve(jwt.decode(token, { complete: true }))
+            .then(t => validateTokenHeader(t))
+            .then(t => getSigningKey(t.header.kid))
+            .then(k => jwt.verify(token, k, { algorithms: ['RS256'] }));
+    }
+
+    function patchUserId(profile) {
+        if (profile && profile.user_id) {
+            profile.user_id = profile.user_id.replace(/^auth0\|/i, "");
+        }
+
+        return profile;
     }
 
     // actual execution flow
 
     rp(getTokenRequestOptions())
-        .then(res => {
-            const idToken = res.id_token;
+        .then(res => decodeAndVerifyToken(res.id_token))
+        .then(p => p.sub)
+        .then(id => {
+            return rp(getClientCredentialsRequestOptions())
+                .then(res => {
+                    const accessToken = res.access_token;
 
-            return Promise.resolve(jwt.decode(idToken, { complete: true }))
-                .then(t => validateTokenHeader(t))
-                .then(t => getSigningKey(t.header.kid))
-                .then(k => jwt.verify(idToken, k, { algorithms: ['RS256'] }));
+                    return decodeAndVerifyToken(accessToken)
+                        .then(() => accessToken);
+                })
+                .then(accessToken => rp(getUserProfileRequestOptions(id, accessToken)));
         })
-        .then(p => callback(null, {
-            id: fixUserId(p.sub),
-            email: p.email,
-            nickname: p.nickname
-        }))
+        .then(p => patchUserId(p))
+        .then(p => callback(null, p))
         .catch(err => callback(new Error(err)));
 }
